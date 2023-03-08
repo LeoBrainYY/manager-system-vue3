e588749e2de7a2aa512db77f526d69ba3ab5bfcf
Crayon <3037686283@qq.com>
2023-03-08 23:03:46 +0800

    encapsulation

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -2,7 +2,7 @@
  * @Author: Crayon 3037686283@qq.com
  * @Date: 2023-02-23 00:10:49
  * @LastEditors: Crayon 3037686283@qq.com
- * @LastEditTime: 2023-03-08 19:57:07
+ * @LastEditTime: 2023-03-08 23:03:20
  * @FilePath: \manager_vue3\manager_-system\src\views\main\system\role\role.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -30,6 +30,7 @@ export default defineComponent({
   },
   setup() {
     return {
+      console.log('test')
       formConfig,
       contentTableConfig
     }
